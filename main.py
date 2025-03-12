ac0cfb22c8b8d0aeb89ca2e57da51c54049ffc13
avrahamm <papaabramg@gmail.com>
2025-03-12 22:23:10 +0200

    stage 3 solution

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 from qdrant_client import QdrantClient, models
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+def get_embedding(text, model):
+    global openai_client
+    text = text.replace("\n", "")
+    return openai_client.embeddings.create(
+        input = [text],
+        model=model
+    ).data[0].embedding
 
 
-def find_nearest_points(collection_name, paper_id, limit=5):
-    global client
-    # scroll_filter={"must": [{"key": "id", "match": {"value": paper_id}}]}
-    scroll_filter = models.Filter(
-        must=[
-            models.FieldCondition(key="id", match=models.MatchValue(value=paper_id)),
-        ]
-    )
-    result = client.scroll(
-        collection_name=collection_name,
-        scroll_filter=scroll_filter,
-        limit=1,
-        with_payload=True,
-        with_vectors=True,
-    )
-    records = result[0]
-
-    # print(type(result[0][0].vector))
-    # print(result[0][0].vector)
-
-    if not records:
-        raise Exception("No results found")
-
-    target_paper_point = records[0]
-    # print(target_paper_point)
-    # print(type(target_paper_point))
-    target_paper_vector = target_paper_point.vector
-
+def find_nearest_points(collection_name, search_vector, limit=5):
+    global qdrant_client
     # Query nearest by target_paper_vector
-    nearest = client.query_points(
+    nearest = qdrant_client.query_points(
         collection_name=collection_name,
-        query=target_paper_vector,
+        query=search_vector,
         limit=limit,
         with_payload=True,
         with_vectors=False,
@@ -40,17 +28,25 @@ def find_nearest_points(collection_name, paper_id, limit=5):
     return nearest.points
 
 
+qdrant_client = QdrantClient(host="localhost", port=6333)
+openai_client = OpenAI()
+
 if __name__ == '__main__':
-    client = QdrantClient(host="localhost", port=6333)
+    input_text = "the attention mechanism in deep learning"
+    model_name = "text-embedding-ada-002"
+    embedding = get_embedding(input_text, model_name)
+
     COLLECTION_NAME = 'arxiv_papers'
-    paper_id = "1311.5068"
     paper_limit = 5
-    nearest_points = find_nearest_points(collection_name=COLLECTION_NAME, paper_id=paper_id, limit=paper_limit)
+    nearest_points = find_nearest_points(
+        collection_name=COLLECTION_NAME,
+        search_vector=embedding,
+        limit=paper_limit
+    )
 
     nearest_points_ids = []
-
     for point in nearest_points:
         nearest_points_ids.append(point.payload["id"])
         # print(point.payload["id"])
-
     print(nearest_points_ids)
+
