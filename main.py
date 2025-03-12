fbe063fabf92393e1673e31a67df09e976299348
avrahamm <papaabramg@gmail.com>
2025-03-12 21:28:12 +0200

    stage 2 solution

--- main.py
+++ main.py
@@ -1,72 +1,56 @@
-# Your code here
 from qdrant_client import QdrantClient, models
-import json
-from typing import Any, Generator
-import uuid
-from qdrant_client.models import PointStruct
-from tqdm import tqdm
-
-def stream_json(file_path) -> Generator[Any, Any, None]:
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            yield json.loads(line)
 
 
-client = QdrantClient(host="localhost", port=6333)
-
-COLLECTION_NAME='arxiv_papers'
-# vectors_config={"size": 1536, "distance": "Cosine"}
-vectors_config=models.VectorParams(
-    size=1536, distance=models.Distance.COSINE
-)
-
-
-if not client.collection_exists(collection_name=f"{COLLECTION_NAME}"):
-    client.create_collection(
-        collection_name=f"{COLLECTION_NAME}",
-        vectors_config=vectors_config,
+def find_nearest_points(collection_name, paper_id, limit=5):
+    global client
+    # scroll_filter={"must": [{"key": "id", "match": {"value": paper_id}}]}
+    scroll_filter = models.Filter(
+        must=[
+            models.FieldCondition(key="id", match=models.MatchValue(value=paper_id)),
+        ]
     )
-
-BATCH_SIZE = 100  # Define your batch size
-
-records_generator = stream_json('data/archive/ml-arxiv-embeddings.json')
-points_batch = []
-
-for index, record in tqdm(enumerate(records_generator)):
-    embedding = record.get("embedding", None)
-    # Skip records with no embedding or invalid embedding
-    if embedding is None:
-        continue
-    if "embedding" in record:
-        del record["embedding"]
-    point = PointStruct(
-        id=str(uuid.uuid5(namespace=uuid.NAMESPACE_DNS, name=record["id"])),
-        vector=embedding,
-        payload=record,
+    result = client.scroll(
+        collection_name=collection_name,
+        scroll_filter=scroll_filter,
+        limit=1,
+        with_payload=True,
+        with_vectors=True,
     )
-    points_batch.append(point)
+    records = result[0]
 
-    if len(points_batch) >= BATCH_SIZE:
-        client.upsert(
-            collection_name=f"{COLLECTION_NAME}",
-            points=points_batch,
-        )
-        print(f"Inserted {len(points_batch)} points, loop index = {index}")
-        points_batch.clear()  # Efficiently clear the list
+    # print(type(result[0][0].vector))
+    # print(result[0][0].vector)
 
-# Process any remaining records in the last batch
-if points_batch:
-    client.upsert(
-        collection_name=f"{COLLECTION_NAME}",
-        points=points_batch,
+    if not records:
+        raise Exception("No results found")
+
+    target_paper_point = records[0]
+    # print(target_paper_point)
+    # print(type(target_paper_point))
+    target_paper_vector = target_paper_point.vector
+
+    # Query nearest by target_paper_vector
+    nearest = client.query_points(
+        collection_name=collection_name,
+        query=target_paper_vector,
+        limit=limit,
+        with_payload=True,
+        with_vectors=False,
     )
-
-print(f"Inserted last {len(points_batch)} points")
-points_batch.clear()  # Efficiently clear the list
+    return nearest.points
 
 
+if __name__ == '__main__':
+    client = QdrantClient(host="localhost", port=6333)
+    COLLECTION_NAME = 'arxiv_papers'
+    paper_id = "1311.5068"
+    paper_limit = 5
+    nearest_points = find_nearest_points(collection_name=COLLECTION_NAME, paper_id=paper_id, limit=paper_limit)
 
+    nearest_points_ids = []
 
+    for point in nearest_points:
+        nearest_points_ids.append(point.payload["id"])
+        # print(point.payload["id"])
 
-
-
+    print(nearest_points_ids)
