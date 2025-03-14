11eca6a4c065ad8ed8cf150c9d53954ee2dc21e5
avrahamm <papaabramg@gmail.com>
2025-03-14 13:47:23 +0200

    stage 4 solution

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from qdrant_client import QdrantClient, models
 from openai import OpenAI
+import re
 import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
+
+def extract_author_name(query_str):
+    author_match = re.match(r".*by\s+([A-Za-z\s\-]+)", query_str)
+    author = author_match.group(1) if author_match else None
+    return author
+
 
 def get_embedding(text, model):
     global openai_client
@@ -15,12 +23,24 @@ def get_embedding(text, model):
     ).data[0].embedding
 
 
-def find_nearest_points(collection_name, search_vector, limit=5):
+def find_nearest_points(collection_name, search_vector, author=None, limit=5):
     global qdrant_client
-    # Query nearest by target_paper_vector
+    # Create filter if author is specified
+    filter_param = None
+    if author:
+        filter_param = models.Filter(
+            must=[
+                models.FieldCondition(
+                    key='authors',
+                    match=models.MatchText(text=author)
+                )
+            ]
+        )
+
     nearest = qdrant_client.query_points(
         collection_name=collection_name,
         query=search_vector,
+        query_filter=filter_param,
         limit=limit,
         with_payload=True,
         with_vectors=False,
@@ -32,15 +52,17 @@ qdrant_client = QdrantClient(host="localhost", port=6333)
 openai_client = OpenAI()
 
 if __name__ == '__main__':
-    input_text = "the attention mechanism in deep learning"
+    target_text = "Mentions of point clouds by Tian-Xing Xu"
+    author_name = extract_author_name(target_text)
     model_name = "text-embedding-ada-002"
-    embedding = get_embedding(input_text, model_name)
+    embedding = get_embedding(target_text, model_name)
 
     COLLECTION_NAME = 'arxiv_papers'
-    paper_limit = 5
+    paper_limit = 3
     nearest_points = find_nearest_points(
         collection_name=COLLECTION_NAME,
         search_vector=embedding,
+        author=author_name,
         limit=paper_limit
     )
 
